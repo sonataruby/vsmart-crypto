45e1e7ee316359c517deac14dd3c6f4ea6e70552
Roller Davic <rollerdavic@gmail.com>
2021-09-24 18:06:51 +0700

    Update

--- telegram.js
+++ telegram.js
@@ -69,7 +69,7 @@ bot.onText(/\/start/, (msg,match) => {
         }]);
     */
     bot.sendPhoto(msg.chat.id,img_url,{
-        caption : "XAUUSD [SHORT]\nOpen : \nSL:\nTP : ",
+        caption : MainConfig.title + " Airdrop<br>Total : "+MainConfig.token.airdrop+"<br>Reward : "+MainConfig.token.airdrop_reward + MainConfig.airdrop.description,
         "reply_markup": {
                 "keyboard": [["Join Start >>","Website"]]
                 }
